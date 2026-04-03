7fcf392eaef2700a2fbb4ff4522885297a409997
Jonathan Setiabudi <107577120+JonathanSetiabudi@users.noreply.github.com>
2026-04-03 02:43:58 +0000

    Initial test

--- src/top.sv
+++ src/top.sv
@@ -1,8 +1,33 @@
 module top (
     /** Input Ports */
+    input wire [3:0] bcd
     /** Output Ports */
+    output logic [6:0] seg7
 );
 
 /** Logic */
+wire a;
+wire b;
+wire c;
+wire d;
+wire e;
+wire f;
+wire g;
+
+assign a = bcd == 4'b0 || bcd == 4'b0010 || bcd == 4'b0011 || bcd == 4'b0101 || bcd == 4'b0110 || bcd == 4'b0111 || bcd == 4'b1000 || bcd == 4'b1001 || bcd == 4'b1010 || bcd == 4'b1100 || bcd == 4'b1110 || bcd == 4'b1111;
+assign b = bcd == 4'b0 || bcd == 4'b0001 || bcd == 4'b0010 || bcd == 4'b0011 || bcd == 4'b0100 || bcd == 4'b0111 || bcd == 4'b1000 || bcd == 4'b1001 || bcd == 4'b1010 || bcd == 4'b1101 || bcd == 4'b0011;
+assign c = bcd == 4'b0 || bcd == 4'b0001 || bcd == 4'b0011 || bcd == 4'b0100 || bcd == 4'b0101 || bcd == 4'b0110 || bcd == 4'b0111 || bcd == 4'b1000 || bcd == 4'b1001 || bcd == 4'b1010 || bcd == 4'b1011 || bcd == 4'b1101;
+assign d = bcd == 4'b0 || bcd == 4'b0010 || bcd == 4'b0011 || bcd == 4'b0101 || bcd == 4'b0110 || bcd == 4'b1000 || bcd == 4'b1001 || bcd == 4'b1011 || bcd == 4'b1100 || bcd == 4'b1101 || bcd == 4'b1110;
+assign e = bcd == 4'b0 || bcd == 4'b0010 || bcd == 4'b0110 || bcd == 4'b1000 || bcd == 4'b1010 || bcd == 4'b1011 || bcd == 4'b1100 || bcd == 4'b1101 || bcd == 4'b1110 || bcd == 4'b1111;
+assign f = bcd == 4'b0 || bcd == 4'b0100 || bcd == 4'b0101 || bcd == 4'b0110 || bcd == 4'b1000 || bcd == 4'b1001 || bcd == 4'b1010 || bcd == 4'b1011 || bcd == 4'b1100 || bcd == 4'b1110 || bcd == 4'b1111;
+assign g = bcd == 4'b0010 || bcd == 4'b0011 || bcd == 4'b0100 || bcd == 4'b0101 || bcd == 4'b0110 || bcd == 4'b1000 || bcd == 4'b1001 || bcd == 4'b1010 || bcd == 4'b1011 || bcd == 4'b1101 || bcd == 4'b1110 || bcd == 4'b1111;
+
+assign seg7[6] = g;
+assign seg7[5] = f;
+assign seg7[4] = e;
+assign seg7[3] = d;
+assign seg7[2] = c;
+assign seg7[1] = b;
+assign seg7[0] = a;
 
 endmodule
--- tb/top_tb.sv
+++ tb/top_tb.sv
@@ -1,5 +1,5 @@
 `include "src/top.sv"
-`default_nettype none
+timescale 1ns/1ps
 
 module top_tb;
 logic clk;
@@ -8,9 +8,11 @@ top
 (
     .clk(clk)
     /** add additional signals */
+    wire [3:0] bcd,
+    logic [6:0] seg7
 );
 
-localparam CLK_PERIOD = /** clk period */;
+localparam CLK_PERIOD = 100;
 always #(CLK_PERIOD/2) clk=~clk;
 
 initial begin
@@ -22,6 +24,22 @@ initial begin
     clk<=1'b1;
     #(CLK_PERIOD*3);
     // testbench logic goes below
+        bcd <= 4'b0000; #(CLK_PERIOD);
+        bcd <= 4'b0001; #(CLK_PERIOD);
+        bcd <= 4'b0010; #(CLK_PERIOD);
+        bcd <= 4'b0011; #(CLK_PERIOD);
+        bcd <= 4'b0100; #(CLK_PERIOD);
+        bcd <= 4'b0101; #(CLK_PERIOD);
+        bcd <= 4'b0110; #(CLK_PERIOD);
+        bcd <= 4'b0111; #(CLK_PERIOD);
+        bcd <= 4'b1000; #(CLK_PERIOD);
+        bcd <= 4'b1001; #(CLK_PERIOD);
+        bcd <= 4'b1010; #(CLK_PERIOD);
+        bcd <= 4'b1011; #(CLK_PERIOD);
+        bcd <= 4'b1100; #(CLK_PERIOD);
+        bcd <= 4'b1101; #(CLK_PERIOD);
+        bcd <= 4'b1110; #(CLK_PERIOD);
+        bcd <= 4'b1111; #(CLK_PERIOD);
     $finish;
 end
 
